14048db16709545322fc67d8df55674a16310809
Nimesh Savaliya <nksavaliya3455@gmail.com>
2021-11-02 15:40:05 +0530

    adding mine

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const width = 10;
+const bombCount = 20;
 const squares = [];
 
 const grid = document.querySelector('.grid');
+
+const bombsArray = new Array(bombCount).fill('bomb');
+const emptyArray = new Array(width * width - bombCount).fill('valid');
+const gameArray = emptyArray.concat(bombsArray);
+
+const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+
+console.log(shuffledArray);
 
 for (let i = 0; i < width * width; i++) {
   const square = document.createElement('div');
 
   square.setAttribute('id', i);
+  square.classList.add(shuffledArray[i]);
 
-  grid.append(square);
+  grid.appendChild(square);
   squares.push(square);
+
+  square.addEventListener('click', () => clicked(square));
 }
-console.log(squares);
+
+const clicked = (square) => {
+  square.classList.add('selected');
+  console.log('clicked');
+};
